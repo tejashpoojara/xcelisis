8e187c1113c5ec32a884355483f8d9132792da1d
tejashpoojara <tejash.poojara@gmail.com>
2016-05-30 22:25:50 +0530

    First Version
    First Version

---
+++ Excelisys/js/tabs.js
@@ -0,0 +1,1 @@
+(function($){$(function(){$('ul.tabs').each(function(){$(this).find('li').each(function(i){$(this).hover(function(){$(this).addClass('current').siblings().removeClass('current').parents('div.section').find('div.box').hide().end().find('div.box:eq('+i+')').fadeIn(10);});});});})})(jQuery)
